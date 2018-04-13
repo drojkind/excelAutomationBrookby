8dd8ae59c14ca451e93bdee3fdbe4a4865a089dc
Daniel Rojkind <daniel@dpc.io>
2018-04-13 16:01:56 +1200

    function to figure out distance based on vehicle rego, trip start and end time using the blackhawk API

--- utils/index.js
+++ utils/index.js
@@ -1,3 +1,4 @@
+const dotenv = require('dotenv').config();
 const fs = require('fs');
 const _ = require('lodash');
 const moment = require('moment');
@@ -5,6 +6,8 @@ const Json2csvParser = require('json2csv').Parser;
 const XLSX = require('xlsx');
 const brookbyKeys = require('./utils/keys/brookby.json');
 const coordinates = require('./utils/keys/coordinates.json');
+const deviceId = require('./utils/keys/deviceid.json');
+const tripData = require('./utils/keys/tripData.json');
 
 const currentPath = process.cwd();
 
@@ -12,10 +15,21 @@ let dateKeys = []; // Key showing start index for a date range and the date in f
 let dateArray = [];
 let selectedDateArray = [];
 let dateArrayConverted = [];
+let distanceArray = [];
 
-function weGetData(data, data2) {
-  return data + data2;
-}
+console.log(tripData.map(data => data.TravelledTrip.StartTime));
+tripData.map((data) => {
+  console.log(`
+  TestIsoString: ${new Date('2018-03-26T07:01:35.000').toISOString()}
+  TestIsoString END: ${new Date('2018-03-26T09:58:09.000').toISOString()}
+  Biger than?: ${new Date(data.TravelledTrip.StartTime).toISOString() < new Date(data.TravelledTrip.EndTime).toISOString()}
+  End Time ISO: ${new Date(data.TravelledTrip.EndTime).toISOString()}
+  Start time local: ${new Date(data.TravelledTrip.StartTime)}
+  End time local: ${new Date(data.TravelledTrip.EndTime)}
+  End time: ${new Date(data.TravelledTrip.EndTime) < new Date(data.TravelledTrip.StartTime)}
+  Duration: ${data.TravelledTrip.Duration}`);
+});
+
 // We get the path for the file that was dragged onto the app.
 function dragData(data) {
   const workbook = XLSX.readFile(data);
@@ -61,6 +75,76 @@ function dragData(data) {
     }
   });
 
+  getTripData(deviceId.JQQ590, '2018-03-26', '2018-03-25T18:01:35.000Z', '2018-03-25T20:58:09.000Z'); // Get trip data function.
+
+  /**
+   * getTripData - description
+   *
+   * @param  {int} vehicleId vehicleID mathced from deviceID
+   * @param  {type} date      date you want to search
+   * @return {type}           returns...nothing currenyl
+   */
+  function getTripData(vehicleId, date, startTime, endTime) {
+    let distanceCount = 0;
+    fetch(`http://webapi.blackhawktracking.com/api/VehicleTrip/Get?vehicleId=${vehicleId}&startDate=${date}T00:00:00.000&endDate=${date}T23:59:59.280&includeGeometries=true&geometryFormat=esrijson`, {
+      headers: {
+        Accept: 'application/json',
+        token: '2A007AC2-4BAD-4625-BF2F-D5DFFE31A44D',
+      },
+      method: 'GET',
+    }).then(response => response.json()).then(j =>
+      j.map(data => data.TravelledTrip).forEach((each, index) => {
+        if (startTime > each.StartTime && endTime < each.EndTime) {
+          j[index].TravelledTrip.WithinSpeed.forEach((each, index) => {
+            distanceArray.push({
+              AverageSpeed: each.AverageSpeed,
+              Distance: each.Distance,
+              StartTime: each.StartTime,
+              EndTime: each.EndTime,
+              type: 'Within speed',
+            });
+          });
+          j[index].TravelledTrip.OverSpeed.forEach((each, index) => {
+            distanceArray.push({
+              AverageSpeed: each.AverageSpeed,
+              Distance: each.Distance,
+              StartTime: each.StartTime,
+              EndTime: each.EndTime,
+              type: 'Over speed',
+            });
+          });
+          j[index].TravelledTrip.OffRoad.forEach((each, index) => {
+            distanceArray.push({
+              AverageSpeed: each.AverageSpeed,
+              Distance: each.Distance,
+              StartTime: each.StartTime,
+              EndTime: each.EndTime,
+              type: 'Off road',
+            });
+          });
+        }
+        const orderedArray = _.orderBy(distanceArray, ['StartTime'], ['asc']);
+        let startIndex;
+        let endIndex;
+
+        _.orderBy(orderedArray).forEach((each, index) => {
+          if (startTime >= each.StartTime && startTime <= each.EndTime) {
+            startIndex = index;
+          }
+          if (endTime >= each.StartTime && endTime <= each.EndTime) {
+            endIndex = index;
+          }
+        });
+
+        orderedArray.slice(startIndex, endIndex).forEach((each, index) => {
+          distanceCount += each.Distance;
+        });
+        console.log('THIS IS THE TOTAL DISTANCE', distanceCount);
+      }));
+    console.log('return distance final', distanceCount);
+  }
+
+
   function outputData(date, currentCount) {
     /**
      * getDistance - Gets distance between a quarry and a site.
@@ -68,7 +152,11 @@ function dragData(data) {
      * @return {type}  distance between site and quarry.
      * Code should use GPS API currently using google distance API...
      */
+
+    console.log(deviceId['5K511']);
+
     function getDistance(site) {
+      console.log(site);
       return fetch(`https://maps.googleapis.com/maps/api/distancematrix/json?units=metric&origins=-36.969591712031935,175.01331160311304&key=AIzaSyD3DrGL7mk0IXupL8BWqoq0pRofJdOeIBc&destinations=${coordinates[site].Position.Longitude},${coordinates[site].Position.Latitude}`).then(response =>
         response.json()).then(j =>
         parseDistance(j.rows[0].elements[0].distance.value) * 2);
@@ -102,13 +190,12 @@ function dragData(data) {
      * @return {type} Distance with proper decimal.
      */
     function parseDistance(distance) {
-      const [length] = distance.toString();
       const string = distance.toString();
-      if (length === 6) {
+      if (string.length === 6) {
         return `${string.substring(0, 3)}.${string.substring(3, 5)}`;
-      } else if (length === 5) {
+      } else if (string.length === 5) {
         return `${string.substring(0, 2)}.${string.substring(2, 4)}`;
-      } else if (length === 4) {
+      } else if (string.length === 4) {
         return `${string.substring(0, 1)}.${string.substring(1, 3)}`;
       }
       return 'Incorrect distance.';
@@ -121,14 +208,19 @@ function dragData(data) {
      * @param  {string} data  description
      * @param  {int} index description
      * @param {string} currentCount the current count of interior array.
+     * @param {boolean} round if noround we dont round the value.
      * @return {string} Return rounded time.
      */
-    function getNextTime(data, index, currentCount) {
+    function getNextTime(data, index, currentCount, round) {
       const sliceFilter = _.filter(selectedDateArray[currentCount]
         .slice(index + 1, selectedDateArray[currentCount].length), {
         Vehicle: data.Vehicle,
       });
-      if (sliceFilter.length !== 0) {
+      if (!round) {
+        if (sliceFilter.length !== 0) {
+          return Promise.resolve(sliceFilter[0]['Time In']);
+        }
+      } else if (sliceFilter.length !== 0) {
         return Promise.resolve(roundTime(sliceFilter[0]['Time In'], 'ceil'));
       }
       return Promise.resolve('MANUAL ENTRY');
@@ -167,12 +259,19 @@ function dragData(data) {
     selectedDateArray[currentCount].forEach((data, index) => {
       dateCount += 1;
       Promise.all([
+        getNextTime(data, index, currentCount, false),
+      ]).then((promise) => {
+        console.log(`time out: ${data['Time Out']} time back: ${promise[0]} vehicle: ${data.Vehicle}`);
+      });
+      Promise.all([
         getDistance(data['Order Name'].substring(0, 4)),
-        getNextTime(data, index, currentCount),
+        getNextTime(data, index, currentCount, true),
         roundTime(data['Time Out'], 'floor'),
         parseWeight(data.Net),
         productNameMatch(data['Product Name']),
       ]).then((promise) => {
+        console.log(deviceId[data.Vehicle]);
+        console.log(moment.utc('2018-03-30T19:47:40.933').valueOf());
         dateArrayConverted.push({
           date,
           quarry: 'KB',
@@ -204,7 +303,6 @@ function dragData(data) {
    * Currently we iterate through each date and make a large file,
    * date selection could be built into the app.
    */
-
   dateKeys.forEach((data, index) => {
     outputData(data[1], index);
   });
